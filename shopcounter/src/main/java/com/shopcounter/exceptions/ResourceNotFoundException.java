216b7c03fb34444683d6ccf0456c14ab8687d5db
Aakash Ohri <aakashohri@hotmail.com>
2020-09-02 09:37:09 +0530

    improved error handling for exception

--- shopcounter/src/main/java/com/shopcounter/exceptions/ResourceNotFoundException.java
+++ shopcounter/src/main/java/com/shopcounter/exceptions/ResourceNotFoundException.java
@@ -1,9 +1,5 @@
 package com.shopcounter.exceptions;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
 public class ResourceNotFoundException extends RuntimeException {
 
     public ResourceNotFoundException(String message) {
